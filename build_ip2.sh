c8aed5da11565bab0e4d5cc024d343398730b5d9
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-04 15:56:53 +0000

    Unified build (20190604) script/makefile ip2

--- build_ip2.sh
+++ build_ip2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
  (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]"
+   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
    exit 1
  }
 
  sys=${1,,}
  [[ $sys == wcoss || $sys == dell || $sys == cray ||\
     $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]"
+   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
    exit 1
  }
  debg=false
@@ -45,8 +45,12 @@
  else
    source ./Conf/Ip2_intel_${sys^}.sh
  fi
+ $CC --version &> /dev/null || {
+   echo "??? IP2: compilers not set." >&2
+   exit 1
+ }
  [[ -z $IP2_VER || -z $IP2_LIB4 ]] && {
-   echo "??? IP2: module/environment not set."
+   echo "??? IP2: module/environment not set." >&2
    exit 1
  }
 
